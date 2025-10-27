486a66857653003cda8b3fd45a252e0b03381e9f
noturavrigk <ksm92038386@gmail.com>
2025-10-27 15:39:36 +0900

    First Commit

---
+++ src/main/java/timeWizard/bilnut/BilnutApplication.java
@@ -0,0 +1,13 @@
+package timeWizard.bilnut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilnutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilnutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/timeWizard/bilnut/BilnutApplicationTests.java
@@ -0,0 +1,13 @@
+package timeWizard.bilnut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilnutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
